e65177f93fac80d74106754d4bf25a8510ea5779
Gilbert Kibet <kibetgilly354@gmail.com>
2023-10-12 00:19:25 +0300

    initializing caterpliar and displaying on planet

--- CaterpillarControlSystem/Program.cs
+++ CaterpillarControlSystem/Program.cs
@@ -2,20 +2,22 @@
 {
     static void Main()
     {
-        Caterpillar caterpillar = new Caterpillar();
+        int planetWidth = 30;
+        int planetHeight = 30;
+        char[,] planet = new char[planetWidth, planetHeight];
+        InitializePlanet(planet);
+
+        Caterpillar caterpillar = new Caterpillar(planet);
 
         while (true)
         {
-            Console.Clear(); 
+            Console.Clear();
+            DisplayPlanet(planet, caterpillar);
 
-            DisplayPlanet(caterpillar); 
-
-            
             Console.WriteLine("Enter a command (U/D/L/R/G/S/Q):");
 
             string input = Console.ReadLine().ToUpper();
 
-           
             switch (input)
             {
                 case "U":
@@ -45,97 +47,33 @@
         }
     }
 
-
-    static void DisplayPlanet(Caterpillar caterpillar)
+    static void InitializePlanet(char[,] planet)
     {
-        int planetWidth = 30;
-        int planetHeight = 30;
-
-        char[,] planet = new char[planetHeight, planetWidth];
-
-        for (int y = 0; y < planetHeight; y++)
+        for (int x = 0; x < planet.GetLength(0); x++)
         {
-            for (int x = 0; x < planetWidth; x++)
+            for (int y = 0; y < planet.GetLength(1); y++)
             {
                 planet[x, y] = '*';
             }
         }
+    }
 
-      
-        planet[0, 11] = '$';
-        planet[0, 29] = '$';
-        planet[1, 3] = '$';
-        planet[1, 12] = '$';
-        planet[2, 8] = '$';
-        planet[4, 5] = '$';
-        planet[6, 6] = '$';
-        planet[8, 0] = '$';
-        planet[9, 6] = '$';
-        planet[9, 20] = '$';
-        planet[10, 7] = '$';
-        planet[11, 22] = '$';
-        planet[14, 8] = '$';
-        planet[15, 0] = '$';
-        planet[16, 13] = '$';
-        planet[18, 4] = '$';
-        planet[19, 6] = '$';
-        planet[19, 11] = '$';
-        planet[19, 24] = '$';
-        planet[20, 9] = '$';
-        planet[21, 1] = '$';
-        planet[21, 25] = '$';
-        planet[22, 16] = '$';
-        planet[24, 7] = '$';
-        planet[25, 13] = '$';
-        planet[26, 21] = '$';
-        planet[27, 14] = '$';
-        planet[28, 2] = '$';
-        planet[28, 22] = '$';
-
-        planet[8, 12] = 'B';
-        planet[10, 16] = 'B';
-        planet[15, 0] = 'B';
-        planet[19, 6] = 'B';
-        planet[19, 11] = 'B';
-        planet[19, 24] = 'B';
-        planet[21, 1] = 'B';
-        planet[21, 25] = 'B';
-        planet[22, 16] = 'B';
-        planet[28, 2] = 'B';
-        planet[28, 22] = 'B';
-
-        planet[0, 0] = 'S'; 
-
-        
-        int headX = caterpillar.GetHeadX();
-        int headY = caterpillar.GetHeadY();
-
-        foreach (var segment in caterpillar.GetSegments())
+    static void DisplayPlanet(char[,] planet, Caterpillar caterpillar)
+    {
+        for (int y = 0; y < planet.GetLength(1); y++)
         {
-            if (headX >= 0 && headX < planetWidth && headY >= 0 && headY < planetHeight)
+            for (int x = 0; x < planet.GetLength(0); x++)
             {
-                planet[headY, headX] = segment[0];
-                headX++; 
-            }
-            else
-            {
-               
-                break;
-            }
-        }
-
-
-        
-        Console.WriteLine("Planet:");
-
-        for (int y = 0; y < planetHeight; y++)
-        {
-            for (int x = 0; x < planetWidth; x++)
-            {
-                Console.Write(planet[x, y]);
+                if (x == caterpillar.GetHeadX() && y == caterpillar.GetHeadY())
+                {
+                    Console.Write(caterpillar.GetSegments()[0][0]);
+                }
+                else
+                {
+                    Console.Write(planet[x, y]);
+                }
             }
             Console.WriteLine();
         }
     }
-
 }
